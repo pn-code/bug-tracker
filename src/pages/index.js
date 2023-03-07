ef0d67981e9be952162ae6d01bdccd669d942a76
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-07 14:10:00 -0800

    Built out Home Page / Dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,35 @@
-import Head from 'next/head'
+import Head from "next/head";
+import DashboardCard from "../components/DashboardCard";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Bug Tracker</title>
-        <meta name="description" content="Track issues with ease!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Bug Tracker</title>
+                <meta name="description" content="Track issues with ease!" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-    </>
-  )
+            {/* Dashboard Home */}
+            <main className="bg-gray-100 h-[90vh] w-full flex flex-col px-4 pt-5 gap-4">
+                <h1 className="text-xl font-bold">Overview</h1>
+                <section className="w-full flex gap-2">
+                    <DashboardCard
+                        title="Projects"
+                        quantity={5}
+                        hrefLink="/projects"
+                    />
+                    <DashboardCard
+                        title="Issues"
+                        quantity={21}
+                        hrefLink="/issues"
+                    />
+                </section>
+            </main>
+        </>
+    );
 }
