e73da321f36b226c13b034ae2a62e16c8a0c715b
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-07 14:10:27 -0800

    Changing table to display text center

--- src/components/IssueCard.jsx
+++ src/components/IssueCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const IssueCard = () => {
     return (
-        <tr className="w-full font-semibold bg-green-200 rounded-md h-20 text-sm sm:text-[16px]">
+        <tr className="w-full font-semibold bg-green-200 rounded-md h-20 text-sm sm:text-[16px] text-center">
             <td>Summary</td>
             <td>Project</td>
             <td>Priority</td>
--- src/pages/issues/index.jsx
+++ src/pages/issues/index.jsx
@@ -4,7 +4,7 @@ import IssueCard from "@/components/IssueCard";
 
 const Issues = () => {
     return (
-        <div className="bg-gray-100 h-[100vh] w-full">
+        <div className="bg-gray-100 h-[100vh] w-[100%]">
             <section className="mx-4 pt-5 flex flex-col gap-4">
                 <header className="flex justify-between items-center">
                     <h1 className="text-xl font-bold">Issues</h1>
@@ -17,7 +17,7 @@ const Issues = () => {
 
                 {/* Sorting Row */}
                 <table className="w-full">
-                    <tr className="text-left bg-gray-300 h-10 text-sm sm:text-[16px]">
+                    <tr className="bg-gray-300 h-10 text-sm sm:text-[16px]">
                         <th>Summary</th>
                         <th>Project</th>
                         <th>Priority</th>
