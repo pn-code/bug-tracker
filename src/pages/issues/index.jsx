06f736c140365a645752dba0cb71906d67459c40
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-06 20:56:18 -0800

    Changing section to table

--- src/pages/issues/index.jsx
+++ src/pages/issues/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import IssueCard from "@/components/IssueCard";
 
 const Issues = () => {
     return (
         <div className="bg-gray-100 h-[100vh] w-full">
-            <section className="mx-4 pt-5">
+            <section className="mx-4 pt-5 flex flex-col gap-4">
                 <header className="flex justify-between items-center">
                     <h1 className="text-xl font-bold">Issues</h1>
                     <div className="text-[16px] font-semibold">
@@ -14,18 +15,17 @@ const Issues = () => {
                 </header>
 
                 {/* Sorting Row */}
-                <section className="my-4 font-semibold bg-gray-300 px-4 py-2 text-sm rounded-md">
-                    <ul className="flex justify-between">
-                        <li>Summary</li>
-                        <li>Project</li>
-                        <li>Priority</li>
-                        <li>Status</li>
-                        <li>Assigned to</li>
-                        <li>Identified Date</li>
-                    </ul>
-                </section>
-
-                
+                <table className="w-full">
+                    <tr className="text-left bg-gray-300 h-10">
+                        <th>Summary</th>
+                        <th>Project</th>
+                        <th>Priority</th>
+                        <th>Status</th>
+                        <th>Assigned User</th>
+                        <th>Date of Origin</th>
+                    </tr>
+                    <IssueCard/>
+                </table>
             </section>
         </div>
     );
