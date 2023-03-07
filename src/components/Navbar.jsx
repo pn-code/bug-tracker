f207f375294a5857c529f2e62a43ebff1ad0cbc5
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-07 13:46:04 -0800

    Updated links on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
-  return (
-    <nav className='flex justify-between px-4 py-6 bg-[#1cba9b] text-white font-semibold items-center'>
-        <h1 className='text-xl'>Bug Tracker</h1>
-        <ul className='flex gap-12 text-sm'>
-            <Link href="">Dashboard</Link>
-            <Link href="">Tasks</Link>
-            <Link href="">Manage</Link>
-            <Link href="">Issues</Link>
-            <Link href="">Login</Link>
-        </ul>
-    </nav>
-  )
-}
+    return (
+        <nav className="flex justify-between px-4 py-6 bg-[#1cba9b] text-white font-semibold items-center">
+            <h1 className="text-xl">Bug Tracker</h1>
+            <ul className="flex gap-12 text-sm">
+                <Link href="/">Dashboard</Link>
+                <Link href="/user-roles">User Roles</Link>
+                <Link href="/issues">Issues</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/profile">Profile</Link>
+            </ul>
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
