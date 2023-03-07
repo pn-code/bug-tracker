aad678fb2000ab42f3e1809bee6dc74114fba900
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-07 14:53:31 -0800

    Built out IssueHistoryCard

---
+++ src/components/IssueHistoryCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const IssueHistoryCard = () => {
+  return (
+    <tr>
+        <td>Property Modified</td>
+        <td>Old Value</td>
+        <td>New Value</td>
+        <td>Date Modified</td>
+    </tr>
+  )
+}
+
+export default IssueHistoryCard
