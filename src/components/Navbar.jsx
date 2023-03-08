298592e78841b1baf03c310b9a3cc7f78cadd979
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-07 19:26:27 -0800

    Adding hamburger menu icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-    return (
-        <nav className="flex justify-between px-4 py-6 bg-[#1cba9b] text-white font-semibold items-center">
-            <h1 className="text-xl">Bug Tracker</h1>
-            <ul className="flex gap-12 text-sm">
-                <Link href="/">Dashboard</Link>
-                <Link href="/roles">User Roles</Link>
-                <Link href="/issues">Issues</Link>
-                <Link href="/projects">Projects</Link>
-                <Link href="/profile">Profile</Link>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="flex justify-between px-4 py-6 bg-[#1cba9b] text-white font-semibold items-center">
+      <h1 className="text-xl">Bug Tracker</h1>
+
+      <button className="sm:hidden">
+        <GiHamburgerMenu size={28}/>
+      </button>
+
+      <ul className="gap-12 text-sm hidden sm:flex">
+        <Link href="/">Dashboard</Link>
+        <Link href="/roles">User Roles</Link>
+        <Link href="/issues">Issues</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/profile">Profile</Link>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
