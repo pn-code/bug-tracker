fd9d998c922b7f9894e05740d4a6ff662f713312
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-08 16:04:35 -0800

    Creating User Table

---
+++ sql/db.sql
@@ -0,0 +1,10 @@
+-- Creating User Table
+CREATE TABLE users (
+    id INT,
+    full_name VARCHAR(50),
+    email VARCHAR(100),
+    role VARCHAR(30),
+    assigned_project INT,
+    created_on timestamp
+);
+
