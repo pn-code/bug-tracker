fda3dfc36aa8cb11f810561d103bb3958cdc12fa
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-08 16:26:50 -0800

    Dropped table & added constraints to table

--- sql/db.sql
+++ sql/db.sql
@@ -1,12 +1,21 @@
 -- Creating User Table
 CREATE TABLE users (
-    id INT,
-    full_name VARCHAR(50),
-    email VARCHAR(100),
-    role VARCHAR(30),
-    assigned_project INT,
-    created_on timestamp
+    id BIGSERIAL NOT NULL,
+    full_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    role VARCHAR(30) NOT NULL,
+    assigned_project INT NOT NULL,
+    created_on timestamp NOT NULL
 );
 
 -- Manually inserting data into our table
 INSERT INTO users (id, full_name, email, role, assigned_project, created_on) VALUES (123, 'Admin', 'admin@gmail.com', 'admin', 0, NOW());
+
+-- To select all data entries from a specific table
+SELECT * from users;
+
+-- To select specific columns to return
+SELECT full_name, role from users;
+
+-- Drop Table
+DROP TABLE users;
