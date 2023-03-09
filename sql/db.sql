bb0d081f01e3319d9192607588851f3aaf5232db
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-09 07:43:01 -0800

    Added additional constraints & added primary key

--- sql/db.sql
+++ sql/db.sql
@@ -1,12 +1,17 @@
 -- Creating User Table
 CREATE TABLE users (
-    id BIGSERIAL NOT NULL,
+    id BIGSERIAL PRIMARY KEY NOT NULL,
     full_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     role VARCHAR(30) NOT NULL,
     assigned_project INT NOT NULL,
     created_on timestamp NOT NULL
 );
+
+-- Additional Constraints
+
+-- For Number Constraints, include:
+check(<number> >=1 and <number> <= 5)
 
 -- Manually inserting data into our table
 INSERT INTO users (full_name, email, role, assigned_project, created_on) VALUES ('Admin', 'admin@gmail.com', 'admin', 0, NOW());
