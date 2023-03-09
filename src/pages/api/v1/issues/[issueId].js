3a0b6b5e99b9d5723ebac590676147c99b6ad88e
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-09 14:03:50 -0800

    Connected individual issue GET endpoint to db

--- src/pages/api/v1/issues/[issueId].js
+++ src/pages/api/v1/issues/[issueId].js
@@ -1,25 +1,39 @@
-export default function handler(req, res) {
+import db from "../../../../../db";
+
+export default async function handler(req, res) {
     if (req.method === "GET") {
-        res.status(200).json({
-            status: "Success",
-            data: {
-                issues: req.query.issueId,
-            },
-        });
+        const results = await db.query(
+            "select * from issues where id = $1",
+            [req.query.issueId]
+        );
+        try {
+            res.status(200).json({
+                status: "Success",
+                results: results.rows.length,
+                data: {
+                    issues: results.rows,
+                },
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Unsuccessful",
+                error,
+            });
+        }
     }
 
     if (req.method === "PUT") {
         res.status(200).json({
             status: "success",
             data: {
-                issues: "Issue 1"
-            }
-        })
+                issues: "Issue 1",
+            },
+        });
     }
 
     if (req.method === "DELETE") {
         res.status(204).json({
-            status: "Success"
-        })
+            status: "Success",
+        });
     }
 }
