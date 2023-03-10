37b36ba94f67e6e7850e33a3f939e2e538d50687
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-09 20:02:54 -0800

    Built out issues POST endpoint

--- src/pages/api/v1/issues/index.js
+++ src/pages/api/v1/issues/index.js
@@ -18,4 +18,47 @@ export default async function handler(req, res) {
             });
         }
     }
+
+    if (req.method === "POST") {
+        try {
+            const {
+                title,
+                description,
+                relatedProject,
+                createdBy,
+                assignedTo,
+                status,
+                priority,
+                targetResolutionDate,
+                actualResolutionDate,
+            } = req.body;
+
+            const results = await db.query(
+                "INSERT INTO issues (title, description, related_project, assigned_to, created_on, created_by, status, priority, target_resolution_date, actual_resolution_date) VALUES ($1, $2, $3, $4, NOW(), $5, $6, $7, $8, $9) returning *",
+                [
+                    title,
+                    description,
+                    relatedProject,
+                    assignedTo,
+                    createdBy,
+                    status,
+                    priority,
+                    targetResolutionDate,
+                    actualResolutionDate
+                ]
+            );
+
+            res.status(201).json({
+                status: "Success",
+                data: {
+                    issue: results.rows[0],
+                },
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Unsuccessful",
+                error,
+            });
+        }
+    }
 }
