ee12240a4c4c5a3e974928a462e20cb3d4898d20
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-09 20:03:05 -0800

    Built out projects POST endpoint

--- src/pages/api/v1/projects/index.js
+++ src/pages/api/v1/projects/index.js
@@ -7,13 +7,37 @@ export default async function handler(req, res) {
             [req.query.id]
         );
         try {
-            res.status(200).json({
+            res.status(201).json({
                 status: "Success",
                 results: results.rows.length,
                 data: {
                     projects: results.rows,
                 },
             });
+        } catch (error) {
+            res.status(500).json({
+                status: "Unsuccessful",
+                error,
+            });
+        }
+    }
+
+    if (req.method === "POST") {
+        try {
+            const { name, createdBy } = req.body;
+
+            const results = await db.query(
+                "INSERT INTO projects (name, created_on, created_by) VALUES ($1, NOW(), $2) returning *",
+                [name, createdBy]
+            );
+
+            res.status(201).json({
+                status: "Success",
+                data: {
+                    user: results.rows[0],
+                },
+            });
+
         } catch (error) {
             res.status(500).json({
                 status: "Unsuccessful",
