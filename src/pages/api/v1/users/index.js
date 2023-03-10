7d475a0b6dd0a04b7744f6e18740a80d8c6bdb13
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-09 19:32:49 -0800

    Built out users POST endpoint

--- src/pages/api/v1/users/index.js
+++ src/pages/api/v1/users/index.js
@@ -4,13 +4,37 @@ export default async function handler(req, res) {
     if (req.method === "GET") {
         try {
             const results = await db.query("select * from users");
-            res.status(200).json({
+            res.status(201).json({
                 status: "Success",
                 results: results.rows.length,
                 data: {
                     users: results.rows,
                 },
             });
+        } catch (error) {
+            res.status(500).json({
+                status: "Unsuccessful",
+                error,
+            });
+        }
+    }
+
+    if (req.method === "POST") {
+        try {
+            const { fullName, email, role } = req.body;
+
+            const results = await db.query(
+                "INSERT INTO users (full_name, email, role, assigned_project, created_on) VALUES ($1, $2, $3, 0, NOW()) returning *",
+                [fullName, email, role]
+            );
+
+            res.status(201).json({
+                status: "Success",
+                data: {
+                    user: results.rows[0],
+                },
+            });
+
         } catch (error) {
             res.status(500).json({
                 status: "Unsuccessful",
