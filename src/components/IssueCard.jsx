ce5e2794817d299e0e0d4af41de161cc8c814f7c
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-10 13:02:37 -0800

    Adding Link component to Issue Details page

--- src/components/IssueCard.jsx
+++ src/components/IssueCard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
-const IssueCard = ({issue}) => {
+const IssueCard = ({ issue }) => {
     return (
         <tr className="w-full font-semibold bg-green-200 rounded-md h-20 text-sm sm:text-[16px] text-center">
-            <td>{issue.title}</td>
+            <td className="hover:underline">
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+            </td>
             <td>{issue.related_project}</td>
             <td>{issue.priority}</td>
             <td>{issue.status}</td>
