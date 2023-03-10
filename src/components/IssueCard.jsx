3041aac55d434a7a675c99a95988d7e691d1cb97
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-10 12:46:44 -0800

    Rendering issues from server

--- src/components/IssueCard.jsx
+++ src/components/IssueCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const IssueCard = () => {
+const IssueCard = ({issue}) => {
     return (
         <tr className="w-full font-semibold bg-green-200 rounded-md h-20 text-sm sm:text-[16px] text-center">
-            <td>Title</td>
-            <td>Project</td>
-            <td>Priority</td>
-            <td>Status</td>
-            <td>Assigned User</td>
-            <td>Identified Date</td>
+            <td>{issue.title}</td>
+            <td>{issue.related_project}</td>
+            <td>{issue.priority}</td>
+            <td>{issue.status}</td>
+            <td>{issue.assigned_to}</td>
+            <td>{issue.created_on.substring(0, 10)}</td>
         </tr>
     );
 };
--- src/pages/issues/index.jsx
+++ src/pages/issues/index.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Link from "next/link";
 import IssueCard from "@/components/IssueCard";
+import serverAPI from "@/api/axios";
 
-const Issues = () => {
+const Issues = ({ issues }) => {
+    console.log(issues);
     return (
         <div className="bg-gray-100 h-[100vh] w-[100%]">
             <section className="mx-4 pt-5 flex flex-col gap-4">
                 <header className="flex justify-between items-center">
                     <h1 className="text-xl font-bold">Issues</h1>
                     <div className="text-[16px] font-semibold">
-                        <Link href="/issues/new" className="bg-blue-400 hover:bg-blue-500 px-4 py-2 text-gray-50 rounded-md">
+                        <Link
+                            href="/issues/new"
+                            className="bg-blue-400 hover:bg-blue-500 px-4 py-2 text-gray-50 rounded-md"
+                        >
                             Add Issue
                         </Link>
                     </div>
@@ -17,15 +22,23 @@ const Issues = () => {
 
                 {/* Sorting Row */}
                 <table className="w-full">
-                    <tr className="bg-gray-300 h-10 text-sm sm:text-[16px]">
-                        <th>Title</th>
-                        <th>Project</th>
-                        <th>Priority</th>
-                        <th>Status</th>
-                        <th>Assigned User</th>
-                        <th>Identified Date</th>
-                    </tr>
-                    <IssueCard/>
+                    <tbody>
+                        <tr className="bg-gray-300 h-10 text-sm sm:text-[16px]">
+                            <th>Title</th>
+                            <th>Project</th>
+                            <th>Priority</th>
+                            <th>Status</th>
+                            <th>Assigned User</th>
+                            <th>Identified Date</th>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        {issues
+                            ? issues.map((issue) => (
+                                  <IssueCard issue={issue} key={issue.id} />
+                              ))
+                            : "No Issues Found"}
+                    </tbody>
                 </table>
             </section>
         </div>
@@ -33,3 +46,14 @@ const Issues = () => {
 };
 
 export default Issues;
+
+export async function getServerSideProps() {
+    const issuesRes = await serverAPI.get("/api/v1/issues");
+
+    // Pass data to the page via props
+    return {
+        props: {
+            issues: issuesRes.data.data.issues,
+        },
+    };
+}
