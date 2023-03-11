deb12e0ef0d114bb4ba96a263efe4095c07633a9
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-10 18:53:47 -0800

    Updated issues PUT endpoint

--- src/pages/api/v1/issues/[id].js
+++ src/pages/api/v1/issues/[id].js
@@ -24,19 +24,41 @@ export default async function handler(req, res) {
     }
 
     if (req.method === "PUT") {
-        res.status(200).json({
-            status: "success",
-            data: {
-                issues: "Issue 1",
-            },
-        });
+        try {
+            const {
+                title,
+                description,
+                relatedProject,
+                createdBy,
+                assignedTo,
+                status,
+                priority,
+                targetResolutionDate,
+                actualResolutionDate,
+            } = req.body;
+
+            const results = await db.query(
+                "UPDATE issues SET title = $2, description = $3, related_project = $4, assigned_to = $6, created_by = $5, status = $7, priority = $8, target_resolution_date = $9, actual_resolution_date = $10 WHERE id = $1",
+                [issueId, title, description, relatedProject, createdBy, assignedTo, status, priority, targetResolutionDate, actualResolutionDate]
+            );
+            
+            res.status(200).json({
+                status: "Success",
+                data: {
+                    issues: results.rows[0],
+                },
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Unsuccessful",
+                error,
+            });
+        }
     }
 
     if (req.method === "DELETE") {
         try {
-            await db.query(
-                `DELETE FROM issues WHERE id = ${issueId}`
-            );
+            await db.query("DELETE FROM issues WHERE id = $1", [issueId]);
             res.status(204).json({
                 status: "Success",
             });
