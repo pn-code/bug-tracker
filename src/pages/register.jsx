9f72739f7c8e8bec9280384fd36d81282524aa92
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-14 08:53:06 -0700

    Handled user register and redirect to login page

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import serverAPI from "@/api/axios";
+import { useRouter } from "next/router";
 
 const Register = () => {
     const [name, setName] = useState("");
@@ -7,9 +9,30 @@ const Register = () => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const router = useRouter();
+
+    const handleRegisterUser = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await serverAPI.post("/api/auth/register", {
+                name,
+                email,
+                password,
+            });
+            if (res.status === 201) {
+                router.push("/login");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="w-full h-[100vh] bg-[#1cba9b] flex items-center justify-center">
-            <form className="bg-gray-100 p-10 rounded-md flex flex-col gap-4">
+            <form
+                onSubmit={(e) => handleRegisterUser(e)}
+                className="bg-gray-100 p-10 rounded-md flex flex-col gap-4"
+            >
                 <section>
                     <h1 className="text-3xl font-bold">Bug Tracker</h1>
                     <h2 className="text-[16px] font-semibold">
@@ -19,7 +42,7 @@ const Register = () => {
 
                 <fieldset className="flex flex-col gap-4">
                     <section className="flex flex-col gap-1">
-                    <label htmlFor="name">Full Name:</label>
+                        <label htmlFor="name">Full Name:</label>
                         <input
                             onChange={(e) => setName(e.target.value)}
                             value={name}
@@ -56,7 +79,9 @@ const Register = () => {
                     </section>
 
                     <section className="flex flex-col">
-                        <label htmlFor="confirmPassword">Confirm Password:</label>
+                        <label htmlFor="confirmPassword">
+                            Confirm Password:
+                        </label>
                         <input
                             onChange={(e) => setConfirmPassword(e.target.value)}
                             value={confirmPassword}
