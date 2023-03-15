97addacbadb124c2ed86bbbef145a943d7f4fd4c
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-14 21:07:48 -0700

    Built out fetchUser handler

--- src/pages/roles.jsx
+++ src/pages/roles.jsx
@@ -1,7 +1,25 @@
-import React from "react";
 import UserRoleCard from "../components/UserRoleCard";
+import serverAPI from "@/api/axios";
+import { useEffect, useState } from "react";
 
 const Role = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const res = await serverAPI.get("/api/v1/users");
+      if (res.status === 201) {
+        setUsers(res.data.users);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="bg-gray-100 h-[90vh] w-full flex px-4 pt-5 gap-10 lg:justify-between lg:gap-20 flex-col lg:flex-row">
       <form className="lg:w-[50%] flex flex-col gap-4">
@@ -26,7 +44,9 @@ const Role = () => {
             <th>Email</th>
             <th>Role</th>
           </tr>
-          <UserRoleCard />
+          {users.map((user) => (
+            <UserRoleCard user={user} key={user.id} />
+          ))}
         </table>
       </div>
     </div>
