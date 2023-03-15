5b71e1ed8384439e73f05f22d7dc75ddba1e2b4b
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-15 16:11:44 -0700

    Removing excess code from App

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,12 @@
-import serverAPI from "@/api/axios";
 import "@/styles/globals.css";
-import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import { UserContext } from "../contexts/UserContext";
+import { UserProvider} from "../contexts/UserContext";
 
 export default function App({ Component, pageProps }) {
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        async function fetchUser() {
-            const res = await serverAPI.get("/api/auth/refresh")
-            setUser(res.data)
-        }
-        fetchUser();
-    }, [])
-
     return (
-        <UserContext.Provider value={user}>
+        <UserProvider>
             <Navbar />
             <Component {...pageProps} />
-        </UserContext.Provider>
+        </UserProvider>
     );
 }
