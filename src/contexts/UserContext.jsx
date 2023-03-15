9e7f7b847ff343bdbe96a1ac0e8def6fb2b3dec3
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-15 16:11:21 -0700

    Refactored user state and handler

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,25 +1,32 @@
 import { createContext, useContext, useState } from "react";
+import serverAPI from "@/api/axios";
 
-const UserContext = createContext(null);
-
-const useUserContext = () => {
-    const [user, setUser] = useContext(UserContext);
-
-    const handleUser = (value) => {
-        setUser(value);
-    };
-
-    return { value: user, onChange: handleUser };
-};
+const UserContext = createContext();
+const UserUpdateContext = createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
+    async function checkForUserToken() {
+        const res = await serverAPI.get("/api/auth/refresh");
+        setUser(res.data.user);
+    }
+
     return (
-        <UserContext.Provider value={[user, setUser]}>
-            {children}
+        <UserContext.Provider value={user}>
+            <UserUpdateContext.Provider value={checkForUserToken}>
+                {children}
+            </UserUpdateContext.Provider>
         </UserContext.Provider>
     );
 };
 
-export { UserProvider, useUserContext };
+function useUser() {
+    return useContext(UserContext);
+}
+
+function useUserUpdate() {
+    return useContext(UserUpdateContext);
+}
+
+export { UserProvider, useUser, useUserUpdate };
