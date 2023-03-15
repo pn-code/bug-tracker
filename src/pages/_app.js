8c966df307bcb0ae2adc4d4b4957b028f16b93d2
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-15 14:07:03 -0700

    Added UserContext & initial check for refresh token

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,24 @@
+import serverAPI from "@/api/axios";
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import { UserContext } from "../contexts/UserContext";
 
 export default function App({ Component, pageProps }) {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        async function fetchUser() {
+            const res = await serverAPI.get("/api/auth/refresh")
+            setUser(res.data)
+        }
+        fetchUser();
+    }, [])
+
     return (
-        <>
+        <UserContext.Provider value={user}>
             <Navbar />
             <Component {...pageProps} />
-        </>
+        </UserContext.Provider>
     );
 }
