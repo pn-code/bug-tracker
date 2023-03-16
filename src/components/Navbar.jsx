ad2ac6468ea250572043314295f3d16def2dc6f1
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-15 17:24:23 -0700

    Added check for refresh token to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useUserContext } from "@/contexts/UserContext";
+import { useUser } from "@/contexts/UserContext";
+import serverAPI from "@/api/axios";
 
 const Navbar = () => {
     const [openNavMenu, setOpenNavMenu] = useState(false);
-    const user = useUserContext();
+    const user = useUser()[0];
+    const setUser = useUser()[1];
+
+    useEffect(() => {
+        // Check for refresh token
+        async function fetchRefreshToken() {
+            const res = await serverAPI.get("/api/auth/refresh");
+            setUser(res.data)
+        }
+
+        try {
+            fetchRefreshToken();
+        } catch (error) {
+            console.error(error);
+        }
+    }, []);
 
     return (
         <nav className="flex justify-between px-4 py-6 bg-[#1cba9b] text-white font-semibold items-center">
@@ -25,71 +41,68 @@ const Navbar = () => {
             {!user && <Link href="/login">Login</Link>}
 
             {/* Mobile NavMenu */}
-            {openNavMenu &&
-                user(
-                    <div className="fixed bg-[#1cba9b] h-[100vh] w-full top-0 left-0 flex items-center justify-center">
-                        <div className="w-[290px] flex flex-col mx-3 gap-12">
-                            <section className="flex justify-between w-full">
-                                <h1 className="text-3xl font-bold">
-                                    Bug Tracker
-                                </h1>
-                                <button
+            {openNavMenu && user && (
+                <div className="fixed bg-[#1cba9b] h-[100vh] w-full top-0 left-0 flex items-center justify-center">
+                    <div className="w-[290px] flex flex-col mx-3 gap-12">
+                        <section className="flex justify-between w-full">
+                            <h1 className="text-3xl font-bold">Bug Tracker</h1>
+                            <button
+                                onClick={() => setOpenNavMenu(false)}
+                                className="text-xl font-bold hover:bg-gray-50 hover:text-[#1cba9b] rounded-full px-3 py-1"
+                            >
+                                X
+                            </button>
+                        </section>
+                        <ul className="flex flex-col gap-8 text-2xl">
+                            <li className="w-full">
+                                <Link
+                                    className="hover:bg-[#29947e] px-2 py-2 rounded-md"
                                     onClick={() => setOpenNavMenu(false)}
-                                    className="text-xl font-bold hover:bg-gray-50 hover:text-[#1cba9b] rounded-full px-3 py-1"
+                                    href="/"
                                 >
-                                    X
-                                </button>
-                            </section>
-                            <ul className="flex flex-col gap-8 text-2xl">
-                                <li className="w-full">
-                                    <Link
-                                        className="hover:bg-[#29947e] px-2 py-2 rounded-md"
-                                        onClick={() => setOpenNavMenu(false)}
-                                        href="/"
-                                    >
-                                        Dashboard
-                                    </Link>
-                                </li>
-                                <li className="w-full">
-                                    <Link
-                                        className="hover:bg-[#29947e] px-2 py-2 rounded-md"
-                                        onClick={() => setOpenNavMenu(false)}
-                                        href="/roles"
-                                    >
-                                        Roles
-                                    </Link>
-                                </li>
-                                <li className="w-full">
-                                    <Link
-                                        className="hover:bg-[#29947e] px-2 py-2 rounded-md"
-                                        onClick={() => setOpenNavMenu(false)}
-                                        href="/issues"
-                                    >
-                                        Issues
-                                    </Link>
-                                </li>
-                                <li className="w-full">
-                                    <Link
-                                        className="hover:bg-[#29947e] px-2 py-2 rounded-md"
-                                        onClick={() => setOpenNavMenu(false)}
-                                        href="/projects"
-                                    >
-                                        Projects
-                                    </Link>
-                                </li>
-                                <li className="w-full">
-                                    <Link
-                                        className="hover:bg-[#29947e] px-2 py-2 rounded-md"
-                                        onClick={() => setOpenNavMenu(false)}
-                                        href="/profile"
-                                    >
-                                        Profile
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+                                    Dashboard
+                                </Link>
+                            </li>
+                            <li className="w-full">
+                                <Link
+                                    className="hover:bg-[#29947e] px-2 py-2 rounded-md"
+                                    onClick={() => setOpenNavMenu(false)}
+                                    href="/roles"
+                                >
+                                    Roles
+                                </Link>
+                            </li>
+                            <li className="w-full">
+                                <Link
+                                    className="hover:bg-[#29947e] px-2 py-2 rounded-md"
+                                    onClick={() => setOpenNavMenu(false)}
+                                    href="/issues"
+                                >
+                                    Issues
+                                </Link>
+                            </li>
+                            <li className="w-full">
+                                <Link
+                                    className="hover:bg-[#29947e] px-2 py-2 rounded-md"
+                                    onClick={() => setOpenNavMenu(false)}
+                                    href="/projects"
+                                >
+                                    Projects
+                                </Link>
+                            </li>
+                            <li className="w-full">
+                                <Link
+                                    className="hover:bg-[#29947e] px-2 py-2 rounded-md"
+                                    onClick={() => setOpenNavMenu(false)}
+                                    href="/profile"
+                                >
+                                    Profile
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
-                )}
+                </div>
+            )}
 
             {user && (
                 <ul className="gap-12 text-sm hidden sm:flex">
