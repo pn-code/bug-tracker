42abc712696980a22af691ad64de22defa15d98f
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-15 17:29:22 -0700

    Updated GET refresh to include user info in json

--- src/pages/api/auth/refresh.js
+++ src/pages/api/auth/refresh.js
@@ -4,38 +4,40 @@ const jwt = require("jsonwebtoken");
 const cookie = require("cookie");
 
 export default async function refreshHandler(req, res) {
-  if (req.method === "GET") {
-    const cookies = req.cookies;
+    if (req.method === "GET") {
+        const cookies = req.cookies;
 
-    if (!cookies?.jwt) return res.status(401).json({ message: "Unauthorized" });
+        if (!cookies?.jwt)
+            return res.status(401).json({ message: "Unauthorized" });
 
-    const refreshToken = cookies.jwt;
+        const refreshToken = cookies.jwt;
 
-    jwt.verify(
-      refreshToken,
-      process.env.REFRESH_TOKEN_SECRET,
-      async (err, decoded) => {
-        if (err) return res.status(403).json({ message: "Forbidden" });
+        jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+            async (err, decoded) => {
+                if (err) return res.status(403).json({ message: "Forbidden" });
 
-        console.log(decoded);
+                const { rows } = await db.query(
+                    "SELECT id, name, email, role, assigned_project FROM users WHERE id = $1",
+                    [decoded.id]
+                );
 
-        const { rows } = await db.query(
-          "SELECT id, email, role, assigned_project FROM users WHERE id = $1",
-          [decoded.id]
+                const accessToken = jwt.sign(
+                    { id: rows[0].id },
+                    process.env.ACCESS_TOKEN_SECRET,
+                    { expiresIn: "30m" }
+                );
+
+                res.json({
+                    id: rows[0].id,
+                    name: rows[0].name,
+                    email: rows[0].email,
+                    role: rows[0].role,
+                    assigned_project: rows[0].assigned_project,
+                    accessToken,
+                });
+            }
         );
-
-        console.log(rows);
-
-        const accessToken = jwt.sign(
-          { id: rows[0].id },
-          process.env.ACCESS_TOKEN_SECRET,
-          { expiresIn: "30m" }
-        );
-
-        res.json({
-            accessToken
-        })
-      }
-    );
-  }
+    }
 }
