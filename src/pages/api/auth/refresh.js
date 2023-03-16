6c9906800d9df2d4c90f3de2691a6800f4f83b2f
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-15 20:48:54 -0700

    Adding trycatch block and status codes

--- src/pages/api/auth/refresh.js
+++ src/pages/api/auth/refresh.js
@@ -8,34 +8,39 @@ export default async function refreshHandler(req, res) {
         if (!cookies?.jwt)
             return res.status(401).json({ message: "Unauthorized" });
 
-        const refreshToken = cookies.jwt;
+        try {
+            const refreshToken = cookies.jwt;
 
-        jwt.verify(
-            refreshToken,
-            process.env.REFRESH_TOKEN_SECRET,
-            async (err, decoded) => {
-                if (err) return res.status(403).json({ message: "Forbidden" });
+            jwt.verify(
+                refreshToken,
+                process.env.REFRESH_TOKEN_SECRET,
+                async (err, decoded) => {
+                    if (err)
+                        return res.status(403).json({ message: "Forbidden" });
 
-                const { rows } = await db.query(
-                    "SELECT id, name, email, role, assigned_project FROM users WHERE id = $1",
-                    [decoded.id]
-                );
+                    const { rows } = await db.query(
+                        "SELECT id, name, email, role, assigned_project FROM users WHERE id = $1",
+                        [decoded.id]
+                    );
 
-                const accessToken = jwt.sign(
-                    { id: rows[0].id },
-                    process.env.ACCESS_TOKEN_SECRET,
-                    { expiresIn: "30m" }
-                );
+                    const accessToken = jwt.sign(
+                        { id: rows[0].id },
+                        process.env.ACCESS_TOKEN_SECRET,
+                        { expiresIn: "30m" }
+                    );
 
-                res.json({
-                    id: rows[0].id,
-                    name: rows[0].name,
-                    email: rows[0].email,
-                    role: rows[0].role,
-                    assigned_project: rows[0].assigned_project,
-                    accessToken,
-                });
-            }
-        );
+                    res.status(200).json({
+                        id: rows[0].id,
+                        name: rows[0].name,
+                        email: rows[0].email,
+                        role: rows[0].role,
+                        assigned_project: rows[0].assigned_project,
+                        accessToken,
+                    });
+                }
+            );
+        } catch (error) {
+            res.status(405).json(error);
+        }
     }
 }
