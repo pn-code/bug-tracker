bf32a69ccf6e4cb91f62d32e49d759c172c34c7d
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-15 17:24:08 -0700

    Refactor UserContext to remove nesting contexts

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,22 +1,13 @@
 import { createContext, useContext, useState } from "react";
-import serverAPI from "@/api/axios";
 
 const UserContext = createContext();
-const UserUpdateContext = createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
-    async function checkForUserToken() {
-        const res = await serverAPI.get("/api/auth/refresh");
-        setUser(res.data.user);
-    }
-
     return (
-        <UserContext.Provider value={user}>
-            <UserUpdateContext.Provider value={checkForUserToken}>
+        <UserContext.Provider value={[user, setUser]}>
                 {children}
-            </UserUpdateContext.Provider>
         </UserContext.Provider>
     );
 };
@@ -25,8 +16,4 @@ function useUser() {
     return useContext(UserContext);
 }
 
-function useUserUpdate() {
-    return useContext(UserUpdateContext);
-}
-
-export { UserProvider, useUser, useUserUpdate };
+export { UserProvider, useUser };
