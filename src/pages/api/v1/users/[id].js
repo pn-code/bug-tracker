791585e6b9b0c390c523aec6982540a61840022d
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-16 18:17:24 -0700

    Refactored, removed DELETE, & updated PUT

--- src/pages/api/v1/users/[id].js
+++ src/pages/api/v1/users/[id].js
@@ -2,17 +2,15 @@ import db from "../../../../../db";
 
 export default async function handler(req, res) {
     if (req.method === "GET") {
-        const results = await db.query(
-            "select * from users where id = $1",
-            [req.query.id]
-        );
+        const { rows } = await db.query("SELECT * FROM users WHERE id = $1", [
+            req.query.id,
+        ]);
+
         try {
             res.status(200).json({
                 status: "Success",
-                results: results.rows.length,
-                data: {
-                    issues: results.rows[0],
-                },
+                results: rows.length,
+                issues: rows[0],
             });
         } catch (error) {
             res.status(500).json({
@@ -23,17 +21,10 @@ export default async function handler(req, res) {
     }
 
     if (req.method === "PUT") {
-        res.status(200).json({
-            status: "success",
-            data: {
-                user: "User 1",
-            },
-        });
-    }
+        await db.query("UPDATE users SET role = $2 WHERE id = $1", [
+            req.query.id, req.body.role
+        ]);
 
-    if (req.method === "DELETE") {
-        res.status(204).json({
-            status: "Success",
-        });
+        res.status(204).end();
     }
 }
