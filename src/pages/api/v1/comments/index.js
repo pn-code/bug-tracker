f31b12ea198b1249ef770ee028894a636f33f0b1
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-18 16:16:22 -0700

    Returning new comment as json in response

--- src/pages/api/v1/comments/index.js
+++ src/pages/api/v1/comments/index.js
@@ -1,16 +1,17 @@
 import db from "../../../../../db";
 
 export default async function commentsHandler(req, res) {
-    const { user_id, issue_id, content } = req.body;
+  const { user_id, issue_id, content } = req.body;
 
-    if (req.method === "POST") {
-        await db.query(
-            "INSERT INTO comments (user_id, issue_id, content, created_on) VALUES ($1, $2, $3, NOW())",
-            [user_id, issue_id, content]
-        );
+  if (req.method === "POST") {
+    const { rows } = await db.query(
+      "INSERT INTO comments (user_id, issue_id, content, created_on) VALUES ($1, $2, $3, NOW()) RETURNING *",
+      [user_id, issue_id, content]
+    );
 
-        res.status(200).json({
-            success: true,
-        });
-    }
+    res.status(200).json({
+      success: true,
+      comment: rows[0],
+    });
+  }
 }
