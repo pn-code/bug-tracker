76f3c877c19cf9f1d0ed221676f1998a8ea30cbb
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-20 12:23:30 -0700

    Changing itemsPerPage to 10

--- src/pages/issues/index.jsx
+++ src/pages/issues/index.jsx
@@ -7,7 +7,7 @@ import Pagination from "@/components/Pagination";
 const Issues = ({ issues }) => {
     // Setting up pagination
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 2;
+    const itemsPerPage = 10;
 
     // Get current issues
     const indexOfLastIssue = currentPage * itemsPerPage;
