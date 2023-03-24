acc7f88dc232d2cdd0f30e802a48c2eb2ecf07bb
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-24 12:58:14 -0700

    Refactored UserContext to include extra states

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,13 +1,40 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import serverAPI from "@/api/axios";
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const authenticate = (userData) => {
+        setIsLoading(false);
+        setUser(userData);
+        setIsAuthenticated(true);
+    };
+
+    useEffect(() => {
+        // Call your API to check user authentication status
+        const checkForUser = async () => {
+            try {
+                const res = await serverAPI.get("/api/auth/refresh");
+                if (res.status === 200) {
+                    authenticate(res.data);
+                }
+            } catch (error) {
+                setIsLoading(false);
+                console.error(error);
+            }
+        };
+        checkForUser();
+    }, []);
 
     return (
-        <UserContext.Provider value={[user, setUser]}>
-                {children}
+        <UserContext.Provider
+            value={{ user, isAuthenticated, isLoading, authenticate }}
+        >
+            {children}
         </UserContext.Provider>
     );
 };
