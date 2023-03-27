6f4bf5c6501664ca326e5adcb436c2b4880018a8
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-27 11:52:58 -0700

    Implementing newly joined users on projects table

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 const ProjectCard = ({ project }) => {
+    console.log(project)
     return (
         <tr className="w-full font-semibold bg-green-200 rounded-md h-20 text-sm sm:text-[16px] text-center">
             <td>
@@ -13,7 +14,7 @@ const ProjectCard = ({ project }) => {
                 </Link>
             </td>
             <td>{project.name}</td>
-            <td>{project.created_by}</td>
+            <td>{`${project.user_name} (${project.created_by})`}</td>
             <td>{project.created_on.substring(0, 10)}</td>
         </tr>
     );
--- src/pages/api/v1/projects/index.js
+++ src/pages/api/v1/projects/index.js
@@ -1,41 +1,48 @@
 import db from "../../../../../db";
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    try {
-      const { rows } = await db.query("SELECT * FROM projects");
+    if (req.method === "GET") {
+        try {
+            const { rows } = await db.query(`
+            SELECT 
+              projects.*, 
+              users.name AS user_name
+            FROM 
+              projects 
+              JOIN users ON projects.created_by::bigint = users.id
+            `);
 
-      res.status(201).json({
-        success: true,
-        results: rows.length,
-        projects: rows,
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        error,
-      });
+            res.status(201).json({
+                success: true,
+                results: rows.length,
+                projects: rows,
+            });
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                error,
+            });
+        }
     }
-  }
 
-  if (req.method === "POST") {
-    try {
-      const { name, createdBy } = req.body;
+    if (req.method === "POST") {
+        try {
+            const { name, createdBy } = req.body;
 
-      const { rows } = await db.query(
-        "INSERT INTO projects (name, created_on, created_by) VALUES ($1, NOW(), $2) RETURNING *",
-        [name, createdBy]
-      );
+            const { rows } = await db.query(
+                "INSERT INTO projects (name, created_on, created_by) VALUES ($1, NOW(), $2) RETURNING *",
+                [name, createdBy]
+            );
 
-      res.status(201).json({
-        success: true,
-        user: rows[0],
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: true,
-        error,
-      });
+            res.status(201).json({
+                success: true,
+                user: rows[0],
+            });
+        } catch (error) {
+            res.status(500).json({
+                success: true,
+                error,
+            });
+        }
     }
-  }
 }
