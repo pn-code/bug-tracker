34b12147608e44868dfbe7045cf8a57d3bd9b40f
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-31 13:58:59 -0700

    Vercel PostgreSQL setup

--- db/index.js
+++ db/index.js
@@ -1,7 +1,9 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
 
 module.exports = {
-    query: (text, params) => pool.query(text, params),
+  query: (text, params) => pool.query(text, params),
 };
