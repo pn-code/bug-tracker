b698d7357596f94b5d1a3310b1498b7b33b2aa8a
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-06 10:06:58 -0700

    Added comment validation on the back-end

--- src/pages/api/v1/comments/index.js
+++ src/pages/api/v1/comments/index.js
@@ -4,14 +4,16 @@ export default async function commentsHandler(req, res) {
   const { user_id, issue_id, content } = req.body;
 
   if (req.method === "POST") {
-    const { rows } = await db.query(
-      "INSERT INTO comments (user_id, issue_id, content, created_on) VALUES ($1, $2, $3, NOW()) RETURNING *",
-      [user_id, issue_id, content]
-    );
+    if (content.trim().length > 0) {
+      const { rows } = await db.query(
+        "INSERT INTO comments (user_id, issue_id, content, created_on) VALUES ($1, $2, $3, NOW()) RETURNING *",
+        [user_id, issue_id, content]
+      );
 
-    res.status(200).json({
-      success: true,
-      comment: rows[0],
-    });
+      res.status(200).json({
+        success: true,
+        comment: rows[0],
+      });
+    }
   }
 }
