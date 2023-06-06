ad20c264eecbe05b57c6bab7576ab6edc1418e2c
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-05 21:25:15 -0700

    Added error message for invalid credentials

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,81 +5,90 @@ import { useRouter } from "next/router";
 import { useUser } from "../contexts/UserContext";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formError, setFormError] = useState("");
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const authenticateUser = useUser().authenticate
+  const authenticateUser = useUser().authenticate;
 
-    const handleUserLogin = async (e) => {
-        e.preventDefault();
+  const handleUserLogin = async (e) => {
+    e.preventDefault();
 
-        try {
-            const res = await serverAPI.post("/api/auth/login", {
-                email,
-                password,
-            });
-            console.log(res)
-            if (res.status === 200){
-                authenticateUser(res.data)
-            }
-            router.push("/");
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    try {
+      const res = await serverAPI.post("/api/auth/login", {
+        email,
+        password,
+      });
 
-    return (
-        <div className="w-full h-[92vh] flex items-center justify-center">
-            <form
-                onSubmit={(e) => handleUserLogin(e)}
-                className="bg-gray-100 p-10 rounded-md flex flex-col gap-4"
-            >
-                <section>
-                    <h1 className="text-3xl font-bold">Bug Tracker</h1>
-                    <h2 className="text-[16px] font-semibold">User Login</h2>
-                </section>
+      if (res.status === 200) {
+        authenticateUser(res.data);
+        router.push("/");
+      }
+    } catch (error) {
+      if (error.response.status === 400) {
+        const errorMessage = error.response.data.message;
+        setFormError(errorMessage);
+      }
 
-                <fieldset className="flex flex-col gap-4">
-                    <section className="flex flex-col gap-1">
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            onChange={(e) => setEmail(e.target.value)}
-                            value={email}
-                            id="email"
-                            className="p-2 rounded-md"
-                            type="email"
-                            placeholder="email"
-                            required
-                        />
-                    </section>
+      console.error(error);
+    }
+  };
 
-                    <section className="flex flex-col">
-                        <label htmlFor="username">Password:</label>
-                        <input
-                            onChange={(e) => setPassword(e.target.value)}
-                            value={password}
-                            id="password"
-                            className="p-2 rounded-md"
-                            type="password"
-                            placeholder="password"
-                            required
-                        />
-                    </section>
-                </fieldset>
-                <button className="py-4 bg-primary text-gray-50 rounded-md hover:bg-primary/80">
-                    Login
-                </button>
-                <span>
-                    Don&apos;t have an account?{" "}
-                    <Link className="underline" href="/register">
-                        Register here.
-                    </Link>
-                </span>
-            </form>
-        </div>
-    );
+  console.log(formError)
+
+  return (
+    <div className="w-full h-[92vh] flex items-center justify-center">
+      <form
+        onSubmit={(e) => handleUserLogin(e)}
+        className="bg-gray-100 p-10 rounded-md flex flex-col gap-4"
+      >
+        <section>
+          <h1 className="text-3xl font-bold">Bug Tracker</h1>
+          <h2 className="text-[16px] font-semibold">User Login</h2>
+          <span className="text-sm text-red-600 font-bold">{formError}</span>
+        </section>
+
+        <fieldset className="flex flex-col gap-4">
+          <section className="flex flex-col gap-1">
+            <label htmlFor="email">Email:</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              id="email"
+              className="p-2 rounded-md"
+              type="email"
+              placeholder="email"
+              required
+            />
+          </section>
+
+          <section className="flex flex-col">
+            <label htmlFor="username">Password:</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              id="password"
+              className="p-2 rounded-md"
+              type="password"
+              placeholder="password"
+              required
+            />
+          </section>
+        </fieldset>
+        <button className="py-4 bg-primary text-gray-50 rounded-md hover:bg-primary/80">
+          Login
+        </button>
+        <span>
+          Don&apos;t have an account?{" "}
+          <Link className="underline" href="/register">
+            Register here.
+          </Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
