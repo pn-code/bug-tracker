3b0f018a8f8f7b5292ec523ca312250316c09196
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-06 11:31:54 -0700

    Fixed query which joins the users and logs tables

--- src/pages/api/v1/logs/[issueId].js
+++ src/pages/api/v1/logs/[issueId].js
@@ -1,27 +1,26 @@
 import db from "../../../../../db";
 
 export default async function logIssueIdHandler(req, res) {
-    const issueId = req.query.issueId;
+  const issueId = Number(req.query.issueId);
 
-    if (req.method === "GET") {
-        const { rows } = await db.query(
-            `SELECT 
-            logs.*, 
-            users.name AS modified_by_name,
-            users1.name AS new_assigned_to_name
-          FROM 
-            issues 
-            JOIN users ON issues.created_by::bigint = users.id
-            JOIN users users1 ON issues.assigned_to::bigint = users1.id
-            JOIN logs ON issues.id = logs.issue_id
-          WHERE 
-            issues.id = $1`,
-            [issueId]
-        );
+  if (req.method === "GET") {
+    const { rows } = await db.query(
+      `SELECT
+      logs.*,
+      users_assigned.name AS new_assigned_to_name,
+      users_modified.name AS modified_by_name
+    FROM
+      logs
+      LEFT JOIN users AS users_assigned ON logs.new_assigned_to::bigint = users_assigned.id
+      LEFT JOIN users AS users_modified ON logs.modified_by::bigint = users_modified.id
+    WHERE
+      logs.issue_id = $1`,
+      [issueId]
+    );
 
-        res.status(200).json({
-            success: true,
-            logs: rows,
-        });
-    }
+    res.status(200).json({
+      success: true,
+      logs: rows,
+    });
+  }
 }
