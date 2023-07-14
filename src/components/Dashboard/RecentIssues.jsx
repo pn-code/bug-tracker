3384287739bb531694894d5aa290817fb39f6c16
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-14 15:29:06 -0700

    Updated width/height of RecentIssues

--- src/components/Dashboard/RecentIssues.jsx
+++ src/components/Dashboard/RecentIssues.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import RecentIssueCard from "./RecentIssueCard";
 
-export default function RecentIssues({issues}) {
+export default function RecentIssues({ issues }) {
     return (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <h2 className="text-xl mb-4">Recent Issues</h2>
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            User
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Issue
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Priority
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Status
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {issues.map(issue => <RecentIssueCard key={issue.id} issue={issue}/>)}
-                </tbody>
-            </table>
+        <div className="h-full w-full flex-1">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <h2 className="text-xl mb-4">Recent Issues</h2>
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                User
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Issue
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Priority
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Status
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {issues.map((issue) => (
+                            <RecentIssueCard key={issue.id} issue={issue} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
