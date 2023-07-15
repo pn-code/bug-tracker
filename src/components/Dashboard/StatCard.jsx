d549063572c2799c2d442b25b135d56f60f10151
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-15 16:00:11 -0700

    Updated stat card to take closedIssuesPercentage

--- src/components/Dashboard/StatCard.jsx
+++ src/components/Dashboard/StatCard.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { LineChart, Line } from "recharts";
 
-export default function StatCard({ title, stat, data, navLink, color }) {
+export default function StatCard({ title, stat, data, navLink, color, closedIssuesPercentage }) {
     // const datas = [
     //     { name: "Page A", uv: 400, amt: 2400 },
     //     { name: "Page B", uv: 600, amt: 2600 },
@@ -22,7 +22,7 @@ export default function StatCard({ title, stat, data, navLink, color }) {
     console.log(textColor)
 
     return (
-        <article className="flex min-w-[360px] flex-col gap-4 w-full py-4 px-6 border rounded-md border-gray-300">
+        <article className="flex min-h-[180px] min-w-[360px] flex-col gap-4 w-full py-4 px-6 border rounded-md border-gray-300">
             <header className="flex justify-between">
                 <div className="flex gap-2">
                     <BookMarked color={color} />
@@ -37,10 +37,14 @@ export default function StatCard({ title, stat, data, navLink, color }) {
                 <Link href={navLink} className="text-gray-200 underline text-sm hover:text-gray-300">
                     View all
                 </Link>
-                <div className="flex flex-col items-end">
+                {data && <div className="flex flex-col items-end">
                     <div className="flex gap-1 items-center text-lg font-bold text-green-600"><ArrowUp/> {data ? data.length: "21%"}</div>
                     <span className="text-sm text-gray-400">this month</span>
-                </div>
+                </div>}
+                {closedIssuesPercentage && <div className="flex flex-col items-end">
+                    <div className="flex items-center text-lg font-bold text-green-600">{closedIssuesPercentage}%</div>
+                    <span className="text-sm text-gray-400">all time</span>
+                </div>}
             </div>
         </article>
     );
