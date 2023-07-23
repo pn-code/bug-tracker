7f8357873e8f132ecb440b76330b2df48e15b13e
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-22 22:09:46 -0700

    Added minor adjustments to styles for mobile

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -4,12 +4,12 @@ import React from "react";
 export default function LandingPage() {
     return (
         <main className="h-[calc(100vh-76px)] w-full flex justify-center items-center flex-col gap-8 custom-bg">
-            <section className="bg-gray-900/95 flex flex-col gap-10 p-8 rounded-md items-center">
+            <section className="bg-gray-900/95 flex flex-col gap-6 sm:gap-10 p-8 sm:rounded-md items-center">
                 <h1 className=" text-white text-xl sm:text-5xl font-semibold">
                     Keep track of bugs for annihilation.
                 </h1>
                 <Link
-                    className="bg-gray-200 px-10 py-3 rounded-sm text-lg hover:bg-white duration-200 ease-linear"
+                    className="bg-gray-200 px-10 py-3 rounded-sm text-[14px] sm:text-lg hover:bg-white duration-200 ease-linear"
                     href="/register"
                 >
                     Get Started
