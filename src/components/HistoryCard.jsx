85456907ddea8e117e9f5dcdae79cd0e8a81a2a9
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-24 14:19:54 -0700

    Updated to handle null res date

--- src/components/HistoryCard.jsx
+++ src/components/HistoryCard.jsx
@@ -6,7 +6,7 @@ const HistoryCard = ({ log }) => {
             <td>{`${log.modified_by_name} (${log.modified_by})`}</td>
             <td>{log.modified_date.substring(0, 10)}</td>
             <td>{`${log.new_assigned_to_name} (${log.new_assigned_to})`}</td>
-            <td>{log.new_actual_resolution_date.substring(0,10)}</td>
+            <td>{log.new_actual_resolution_date ? log.new_actual_resolution_date.substring(0,10) : "Not resolved"}</td>
             <td>{log.new_status}</td>
         </tr>
     );
