2e338648c543ddd34c14d9e4f48c9f1b171527f4
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-24 14:28:15 -0700

    Improved style on mobile devices

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -34,16 +34,16 @@ const Profile = ({ projects, issues }) => {
     return (
         <main className="h-full sm:h-[90vh] w-full flex flex-col px-4 pt-5 gap-4 text-text">
             <header className="flex justify-between">
-                <h1 className="text-2xl font-bold">User Profile</h1>
+                <h1 className="text-lg sm:text-2xl font-bold">User Profile</h1>
                 <button
                     onClick={() => logoutUser()}
-                    className="w-24 px-4 py-2 bg-red-600/90 text-white rounded-md hover:bg-red-600/80"
+                    className="w-24 px-4 py-1 bg-red-600/90 text-white rounded-md hover:bg-red-600/80"
                 >
                     Log Out
                 </button>
             </header>
 
-            <section>
+            <section className="text-[16px] sm:text-[18px] flex flex-col gap-2">
                 <article>
                     <h2 className="font-semibold">Full Name: </h2>
                     <p className="text-green-300">{user?.name}</p>
