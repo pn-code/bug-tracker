14064f3a474bc526879b4064c9dcc4957316cfea
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-23 19:25:18 -0700

    Hide several columns when using mobile device

--- src/components/Dashboard/RecentIssueCard.jsx
+++ src/components/Dashboard/RecentIssueCard.jsx
@@ -16,16 +16,16 @@ export default function RecentIssueCard({ issue }) {
 
     return (
         <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-            <th
+            <td
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="hidden sm:table-cell px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
                 {issue.created_by_name}
-            </th>
+            </td>
             <td className="px-6 py-4">
                 {issue.title && shortenString(issue.title)}
             </td>
-            <td className="px-6 py-4">{issue.priority && handlePriority(issue.priority)}</td>
+            <td className="hidden sm:table-cell px-6 py-4">{issue.priority && handlePriority(issue.priority)}</td>
             <td className="px-6 py-4">{issue.status}</td>
         </tr>
     );
--- src/components/Dashboard/RecentIssues.jsx
+++ src/components/Dashboard/RecentIssues.jsx
@@ -9,13 +9,13 @@ export default function RecentIssues({ issues }) {
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" className="px-6 py-3">
+                            <th scope="col" className="px-6 py-3 hidden sm:table-cell">
                                 User
                             </th>
                             <th scope="col" className="px-6 py-3">
                                 Issue
                             </th>
-                            <th scope="col" className="px-6 py-3">
+                            <th scope="col" className="px-6 py-3 hidden sm:table-cell">
                                 Priority
                             </th>
                             <th scope="col" className="px-6 py-3">
