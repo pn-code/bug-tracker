bdb97f16a5f935dbbf94e488e7df2474f2d3df70
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-27 12:04:19 -0700

    Changed text to green for contrast

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -4,13 +4,13 @@ const CommentCard = ({ comment }) => {
     const formattedDate = `${comment.created_on.substring(5, 10)}-${comment.created_on.substring(0, 4)}`;
 
     return (
-        <article className="flex flex-col my-2 border-t-2 border-gray-400/50">
+        <article className="flex flex-col my-2 border-y-2 border-gray-400/50 py-2">
             <section className="flex justify-between mb-2">
                 <h2 className="font-semibold">{`${comment.user_name} (${comment.user_id})`}</h2>
                 <p className="font-semibold">{formattedDate}</p>
             </section>
 
-            <p>{comment.content}</p>
+            <p className="text-green-300">{comment.content}</p>
         </article>
     );
 };
