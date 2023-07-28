d16a96af7e81771171caeb5e023498525d691cf6
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-27 18:59:41 -0700

    Created YourAssignedIssues

---
+++ src/components/Dashboard/YourAssignedIssues.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import RecentIssueCard from './RecentIssueCard'
+
+export default function YourAssignedIssues({issues, userId}) {
+  return (
+    <div className="h-full w-full flex-1">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <h2 className="text-xl mb-4">Your Assigned Issues</h2>
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3 hidden sm:table-cell">
+                                Found By
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Issue
+                            </th>
+                            <th scope="col" className="px-6 py-3 hidden sm:table-cell">
+                                Priority
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Status
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* Display 10 most recent issues */}
+                        {issues.filter(issues => issues.assigned_to == userId).slice(0, 10).map((issue) => (
+                            <RecentIssueCard key={issue.id} issue={issue} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+  )
+}
