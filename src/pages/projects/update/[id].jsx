0d8d2143ef468a9eede63136c00bbfd64e0ac085
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-01 13:42:50 -0700

    Created update project page

---
+++ src/pages/projects/update/[id].jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import serverAPI from "@/api/axios";
+
+export default function updateProject({ project }) {
+    const [projectName, setProjectName] = useState(project.name)
+    return (
+        <div className="h-[90vh] w-full text-text">
+            <form
+                onSubmit={(e) => submitProject(e)}
+                className="px-4 pt-5 flex flex-col gap-4"
+            >
+                <header className="flex justify-center sm:justify-between gap-20">
+                    <h1 className="text-2xl font-bold">Update Project</h1>
+                    <div className="text-[16px] font-semibold">
+                        <Link
+                            href="/projects"
+                            className="bg-secondary hover:bg-secondary/80 px-4 py-2 rounded-md text-background"
+                        >
+                            Return
+                        </Link>
+                    </div>
+                </header>
+
+                <fieldset className="flex flex-col gap-4 items-center sm:items-start">
+                    <section className="flex flex-col gap-2">
+                        <label htmlFor="project">Project Name: </label>
+                        <input
+                            onChange={(e) => setProjectName(e.target.value)}
+                            value={projectName}
+                            className="px-2 py-1 rounded-md sm:w-72"
+                            id="project"
+                            type="text"
+                            placeholder="project name"
+                        />
+                    </section>
+                    <button className="bg-primary hover:bg-primary/80 text-white rounded-md py-2 w-[290px]">
+                        Update Project
+                    </button>
+                </fieldset>
+            </form>
+        </div>
+    );
+}
+
+export async function getServerSideProps({ params }) {
+    const projectId = params.id;
+    const res = await serverAPI.get(`/api/v1/projects/${projectId}`);
+
+    // Pass data to the page via props
+    return {
+        props: {
+            project: res.data.projects,
+        },
+    };
+}
