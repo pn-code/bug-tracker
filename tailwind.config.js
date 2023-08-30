a69ed774aaa63061a9956f53b6390775f4e05278
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-30 12:04:37 -0700

    Updated bg-color to dark gray

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
     extend: {
       colors: {
         text: "#e6f7fa",
-        background: "#051619",
+        background: "#181818",
         primary: "#307848",
         secondary: "#e2f7fe",
         accent: "#91f7da",
